25e7be78d4daecc5e780b4e1fbe04cc9bd38137f
ProfCowboy <zacharytjones1@gmail.com>
2023-01-20 19:29:45 -0800

    Initial commit

---
+++ Source/BlankTape/BlankTapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlankTapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLANKTAPE_API ABlankTapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
